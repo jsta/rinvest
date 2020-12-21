5770d38c2784b20c8166204bafa3cc4b7ac0f52c
jsta <stachel2@msu.edu>
2020-12-21 13:31:47 -0500

    add preflight ndr checks, stash test data infrastructure

--- R/ndr.R
+++ R/ndr.R
@@ -6,23 +6,8 @@
 #' @export
 #'
 #' @examples \dontrun{
-#' data_dir <- system.file("extdata/NDR", package = "rinvest")
-#' args <- list(
-#'   "workspace_dir" = "workspace",
-#'   "dem_path" = paste0(data_dir, "/DEM_gura.tif"),
-#'   "lulc_path" = paste0(data_dir, "/land_use_gura.tif"),
-#'   "runoff_proxy_path" = paste0(data_dir, "/precipitation_gura.tif"),
-#'   "watersheds_path" = paste0(data_dir,  "/watershed_gura.shp"),
-#'   "biophysical_table_path" = paste0(data_dir,  "/biophysical_table_gura.csv"),
-#'   "calc_p" = TRUE,
-#'   "calc_n" = FALSE,
-#'   "threshold_flow_accumulation" = 1000,
-#'   "k_param" = 2,
-#'   "subsurface_eff_p" = 0.5,
-#'   "subsurface_critical_length_p" = 25
-#' )
 #'
-#' ndr(args)
+#' ndr(ndr_testdata_args, overwrite = TRUE)
 #'
 #' }
 ndr <- function(args, overwrite = FALSE){
@@ -35,6 +20,8 @@ ndr <- function(args, overwrite = FALSE){
   if(dir.exists(workspace_path) & !overwrite){
     stop("Output folder already exists. Consider setting overwrite option.")
   }
+
+  preflight_checks_ndr(args)
 
   args_py <- reticulate::r_to_py(args)
   ndr <- invest$ndr$ndr
@@ -58,6 +45,36 @@ ndr_p_export_total <- function(ndr_output){
   p_export_path <- ndr_output[which(basename(ndr_output) == "p_export.tif")]
 
   raster::cellStats(raster::raster(p_export_path), "sum")
+}
+
+
+# ndr(ndr_testdata_args, overwrite = TRUE)
+preflight_checks_ndr <- function(args, checks =
+                                   c("int_rasters", "lulc_code_match",
+                                     "raster_extent_match")){
+
+  ## int_rasters
+  # sf::gdal_utils("info", args$lulc_path)
+  if(length(grep("INT", raster::dataType(raster::raster(args$lulc_path)))) == 0){
+    stop("lulc raster not of type integer")
+  }
+
+  # lulc_code_match
+  lulc_raster <- raster::unique(raster::raster(args$lulc_path))
+  lulc_biophys <- read.csv(args$biophysical_table_path, stringsAsFactors = FALSE)$lucode
+  if(!all(lulc_raster %in% lulc_biophys)){
+    stop(paste0(lulc_raster[!(lulc_raster %in% lulc_biophys)],
+                " undefined in biophys table"))
+  }
+
+  # raster_extent_match
+  raster_extents <- lapply(
+    list(args$lulc_path, args$dem_path, args$runoff_proxy_path),
+    function(x) raster::extent(raster::raster(x)))
+  if(!all(sapply(raster_extents, FUN = identical, raster_extents[[1]]))){
+    stop("input rasters have differing spatial extents")
+  }
+
 }
 
 
