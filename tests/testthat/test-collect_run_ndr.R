511edd3b22972782a0aa89013f64672cef51f0a0
jsta <stachel2@msu.edu>
2022-04-23 07:30:37 -0600

    permissions issue with local testing via symlink

--- tests/testthat/test-collect_run_ndr.R
+++ tests/testthat/test-collect_run_ndr.R
@@ -1,6 +1,6 @@
 test_that("collect_run_ndr works", {
   unlink("workspace_temp", recursive = TRUE)
-  collect_run_ndr(ndr_testdata_args(), symlink = TRUE)
+  collect_run_ndr(ndr_testdata_args(), symlink = FALSE)
   # dir("workspace_temp")
   testthat::expect_gt(length(dir("workspace_temp")), 8)
 })
